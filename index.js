f73eeacf0daefce197623cd2dd49ae691ef75d63
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-11-17 14:52:41 +0530

    v1

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000; // Choose a port number
+
+// Middleware to parse incoming JSON requests
+app.use(bodyParser.json());
+
+// Endpoint to handle incoming GPS coordinates
+app.post('/update-location', (req, res) => {
+  const { latitude, longitude } = req.body;
+
+  // Process the coordinates as needed (e.g., store in a database)
+  console.log('Received coordinates:');
+  console.log('Latitude:', latitude);
+  console.log('Longitude:', longitude);
+
+  // Send a response (you can customize the response as needed)
+  res.json({ message: 'Coordinates received successfully' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
